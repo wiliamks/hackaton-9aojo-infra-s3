da9641b23c4e52907eda741be43b975aa4765b98
Anderson Marques <marques_sep@hotmail.com>
2023-04-01 14:07:31 -0300

    Alterado credencial

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAWDZIWJ6V2DPRQ4O2"
-    secret_key = "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"
+    access_key = "AKIAWDZIWJ6V56JUYQA4"
+    secret_key = "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"
 }
